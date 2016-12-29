5384829d13ffc78c975960bafe8482b4f6bae615
Vitor Augusto Leal <vitoraugusto_leal@hotmail.com>
2016-12-29 15:02:12 -0200

    documentation tests

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,66 @@
 defmodule Cards do
+  @moduledoc """
+    Provide methods for creating and handling a deck of cards
+  """
 
+  @doc """
+    Returns a list of strings representing a deck of cards
+  """
   def create_deck do
-    ["Blue-eyes", "Red-eyes", "Dark Magician", "Dark Magician Girl"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  @doc """
+    Determines deck contains a given card
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
+  def contains?(deck, card) do
+     Enum.member?(deck, card)
+  end
+
+  @doc """
+    Devides a deck into a hand and the remainder of the deck
+    The `hand_size` arguments indicates how many cards should be in the hand
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+     binary = :erlang.term_to_binary(deck)
+     File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      { :ok, binary }    -> :erlang.binary_to_term binary
+      { :error, _reason } -> "That file doesn\'t exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck |> Cards.shuffle |> Cards.deal(hand_size)
+  end
+
 end
